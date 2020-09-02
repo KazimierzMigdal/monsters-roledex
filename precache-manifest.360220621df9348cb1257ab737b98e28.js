39277558440a89cf05a2a1ffed4ccfaab521b9b4
KazimierzMigdal <kazmierz.migdal@protonmail.com>
2020-09-02 20:19:21 +0200

    Updates

---
+++ precache-manifest.360220621df9348cb1257ab737b98e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac359e05ed79b74d22bdf207bc8db0f",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "dd07ddc8d95911235f2b",
+    "url": "/monsters-roledex/static/css/main.e68778ee.chunk.css"
+  },
+  {
+    "revision": "18e3087de538d91ee632",
+    "url": "/monsters-roledex/static/js/2.2a71acd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.2a71acd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd07ddc8d95911235f2b",
+    "url": "/monsters-roledex/static/js/main.d4489f68.chunk.js"
+  },
+  {
+    "revision": "190f18fc45d28b0800c4",
+    "url": "/monsters-roledex/static/js/runtime-main.4be1a078.js"
+  }
+]);
